2342dfc6119061676d0d17861543aae2e56cfba3
yvt <i@yvt.jp>
2020-04-28 20:27:42 +0900

    docs: `OutputDebugMessageW` → `OutputDebugStringW`

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-//! A logger that redirects all messages to the [`OutputDebugMessageW`]
+//! A logger that redirects all messages to the [`OutputDebugStringW`]
 //! Win32 API function in a format similar to [`simple_logger`].
 //!
-//! [`OutputDebugMessageW`]: https://docs.microsoft.com/en-us/windows/win32/api/debugapi/nf-debugapi-outputdebugstringw
+//! [`OutputDebugStringW`]: https://docs.microsoft.com/en-us/windows/win32/api/debugapi/nf-debugapi-outputdebugstringw
 //! [`simple_logger`]: https://crates.io/crates/simple_logger
 //!
 //! This crate is useful if you are writing a Windows GUI application, where
 //! stdout and stderr do not work. The messages outputted by
-//! `OutputDebugMessageW` can be monitored using a program such as [DebugView]
+//! `OutputDebugStringW` can be monitored using a program such as [DebugView]
 //! and Visual Studio's "Output" window.
 //!
 //! [DebugView]: https://docs.microsoft.com/en-us/sysinternals/downloads/debugview
